8dbab769f449d44225c95b80c8d116a7d728066b
leonkosarev <70164506+leonkosarev@users.noreply.github.com>
2020-09-25 08:49:10 +0300

    optimize space

--- src/grid_state.py
+++ src/grid_state.py
@@ -6,11 +6,10 @@ class GridState():
     
     def __init__(self, curr_state, goal_state, step, parent = None):
         self.curr_state = curr_state
-        self.goal_state = goal_state
         self.step = step
         self.parent = parent
         self.score = step
-        self.calculate_potential()
+        self.calculate_potential(goal_state)
 
     def __hash__(self):
         return hash(str(self.curr_state))
@@ -36,12 +35,12 @@ class GridState():
     def manhattan_distance(self, x_curr, y_curr, x_goal, y_goal):
         return abs(x_curr-x_goal)+abs(y_curr-y_goal)
     
-    def calculate_potential(self):
+    def calculate_potential(self, goal_state):
         # Calculates the heuristic score of the grid using manhattan distance
         
         for tile in self.curr_state:
             i_curr = self.curr_state.index(tile)
-            i_goal = self.goal_state.index(tile)
+            i_goal = goal_state.index(tile)
             x_curr, y_curr = i_curr // int(np.sqrt(len(self.curr_state))), i_curr % int(np.sqrt(len(self.curr_state)))
             x_goal, y_goal = i_goal // int(np.sqrt(len(self.curr_state))), i_goal % int(np.sqrt(len(self.curr_state)))
             self.score += self.manhattan_distance(x_curr, y_curr, x_goal, y_goal)
